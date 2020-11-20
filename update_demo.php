ffa50b90f33c7b546e3a50ca06e28787fa37c6f7
Mickael MERCIER <ozonex.service@gmail.com>
2020-11-20 10:18:38 +0100

    Update demo

--- update_demo.php
+++ update_demo.php
@@ -9,27 +9,31 @@ $result = $req->fetch();
 $id = (int)$result['id'];
 $ville = (string)$result['ville'];
 
-$req = $bdd->prepare('SELECT timezone FROM horlogerie WHERE id_systeme = :id_systeme');
-$req->execute(array(
-	'id_systeme' => $id));
-$result = $req->fetch();
+for ($i = 0; $i < 55; $i++) {
+	$req = $bdd->prepare('SELECT timezone FROM horlogerie WHERE id_systeme = :id_systeme');
+	$req->execute(array(
+		'id_systeme' => $id));
+	$result = $req->fetch();
 
-$date = new DateTime("now", new DateTimeZone((string)$result['timezone'])); 
+	$date = new DateTime("now", new DateTimeZone((string)$result['timezone'])); 
 
-$req = $bdd->prepare('UPDATE login SET alive = :alive WHERE id_systeme = :id_systeme');
-$req->execute(array(
-	'alive' => $date->format('d/m/Y-H:i'),
-	'id_systeme' => 'DEMO-0001'
-	));
+	$req = $bdd->prepare('UPDATE login SET alive = :alive WHERE id_systeme = :id_systeme');
+	$req->execute(array(
+		'alive' => $date->format('d/m/Y-H:i'),
+		'id_systeme' => 'DEMO-0001'
+		));
 
-if ($date->diff(new DateTime('05:00:00', new DateTimeZone((string)$result['timezone'])))->format('%R') == '+') {
-	$date = get_datetime_from_coordinates(get_coordinates_from_address($ville));
+	if ($date->diff(new DateTime('05:00:00', new DateTimeZone((string)$result['timezone'])))->format('%R') == '+') {
+		$date = get_datetime_from_coordinates(get_coordinates_from_address($ville));
 
-	$req = $bdd->prepare('UPDATE horlogerie SET index_gmt = :index_gmt, timezone = :timezone WHERE id_systeme = :id_systeme'); 
-    $req->execute(array( 
-        'index_gmt' => 'GMT' . $date->format('P'),
-        'timezone' => $date->getTimezone()->getName(),
-        'id_systeme' => $id
-        ));
+		$req = $bdd->prepare('UPDATE horlogerie SET index_gmt = :index_gmt, timezone = :timezone WHERE id_systeme = :id_systeme'); 
+	    $req->execute(array( 
+	        'index_gmt' => 'GMT' . $date->format('P'),
+	        'timezone' => $date->getTimezone()->getName(),
+	        'id_systeme' => $id
+	        ));
+	}
+
+	sleep(60);
 }
 ?>
